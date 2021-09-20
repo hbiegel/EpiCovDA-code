d077cf925b3bc89a397af620c6ef6678a5b0fe42
Hannah Biegel <hbiegel@math.arizona.edu>
2021-09-19 21:26:16 -0600

    Fixed "update_JHU_data.R"
    Can be run from terminal

--- Matlab_manuscript_code/r_code/run_all_forecast_code.sh
+++ Matlab_manuscript_code/r_code/run_all_forecast_code.sh
@@ -1,6 +1,14 @@
 ## bash script to run all components of forecasts
+# Change the working directory to this folder (r_code)
 
-cd /Applications/MATLAB_R2018a.app/bin
+
+# Comment out this line if your data is up-to-date
+# This file could be updated to only download NEW data
+# Currently it downloads all data until TODAY
+Rscript update_JHU_data.R
+
+
+cd /Applications/MATLAB_R2021a.app/bin
 ##  ./matlab -nojvm -nosplash -nodisplay 
 
 ./matlab -nosplash -nodisplay -r "cd /Users/hannah.biegel/Dropbox/Research-Materials-Hannah/covid_related/HRB_COVID_code; mainEpiCovDA; quit"
